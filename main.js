6b8fbcd827087799c24b248d49db501f3efe2a25
Rustamjonov Davron <davronalirustamjonov@gmail.com>
2024-06-06 17:29:06 +0500

    Add

---
+++ main.js
@@ -0,0 +1,26 @@
+const userItem = document.getElementById("user");
+
+function getUser() {
+    fetch('https://autoapi.dezinfeksiyatashkent.uz/api/cities')
+    .then(res => res.json())
+    .then(users => {
+        let x = 0;
+        users.data.map(item => {
+            userItem.innerHTML += `
+                <li>
+                    <p class="counter"> ${++x}</p>
+                    <p class="place-name">${item.name}</p>
+                    <p class="text">${item.text}</p>
+                    <img class="auto-img" src="https://autoapi.dezinfeksiyatashkent.uz/api/uploads/images/${item.image_src}" alt="">
+                    
+                        <button class="edit">Edit</button>
+                        <button class="delete">Delete</button>
+                    
+                </li>
+            `;
+        });
+    });
+    
+}
+
+getUser();
